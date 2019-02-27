bcf6d32d7634549cae6cca857700054a4b703517
Diego Pinheiro <diegopin@gmail.com>
2019-02-27 10:05:07 -0800

    sync

--- maracatu/__init__.py
+++ maracatu/__init__.py
@@ -1,3 +1,4 @@
 
 from maracatu.src.base.scatter import Scatter
-from maracatu.src.base.curves import *
+from maracatu.src.base.curves import Curves
+from maracatu.src.base.error_bar import ErrorBar
